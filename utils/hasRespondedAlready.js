0cc5d140fb805ff4c6d09e70291c008d0f96fcc1
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-10-11 14:08:18 +0800

    Add condition to the new utility function

--- utils/hasRespondedAlready.js
+++ utils/hasRespondedAlready.js
@@ -4,13 +4,15 @@ import axios from 'axios'
 
 export default async (id, fname, lname) => {
   let respondedAlready = false
-  await axios
-    .post(`http://127.0.0.1:8080/topics/${id}/responded/`, {
-      firstname: fname,
-      lastname: lname
-    })
-    .then(res => {
-      if (res.data.responded) respondedAlready = true
-    })
+  if (fname && lname) {
+    await axios
+      .post(`http://127.0.0.1:8080/topics/${id}/responded/`, {
+        firstname: fname,
+        lastname: lname
+      })
+      .then(res => {
+        if (res.data.responded) respondedAlready = true
+      })
+  }
   return respondedAlready
 }
