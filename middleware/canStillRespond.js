3ef6cf234a680bd2b7e02533ac40f80d19d5dff3
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-10-11 11:43:56 +0800

    Do not use the new utility in the middleware

--- middleware/canStillRespond.js
+++ middleware/canStillRespond.js
@@ -2,27 +2,12 @@
 
 import axios from 'axios'
 
-import hasRespondedAlready from '~/utils/hasRespondedAlready.js'
-
 export default async function({ route, redirect }) {
   await axios
     .get(`http://127.0.0.1:8080/topics/${route.params.id}/`)
-    .then(async res => {
-      if (res.data.done) return redirect({ name: 'topics' })
+    .then(res => {
+      if (res.data.done) redirect({ name: 'topics' })
       else if (route.query.fname & route.query.lname)
-        if (
-          await hasRespondedAlready(
-            route.params.id,
-            route.query.fname,
-            route.lname
-          )
-        ) {
-          return redirect({ name: 'topics' })
-        }
-      return redirect({
-        name: 'respond',
-        params: route.params,
-        query: route.query
-      })
+        redirect({ name: 'respond', params: route.params, query: route.query })
     })
 }
